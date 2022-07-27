58a288d754adf36bace6c9b67be50bca4c0ec3aa
Finn Roblin <finnrobl@amazon.com>
2022-07-27 13:49:36 -0700

    Implemented CSV Processor w unit tests, validated that delimiter and quote char are different
    Signed-off-by: Finn Roblin <finnrobl@amazon.com>

--- data-prepper-plugins/csv-processor/src/main/java/com/amazon/dataprepper/plugins/processor/csv/CSVProcessorConfig.java
+++ data-prepper-plugins/csv-processor/src/main/java/com/amazon/dataprepper/plugins/processor/csv/CSVProcessorConfig.java
@@ -66,4 +66,9 @@ public class CSVProcessorConfig {
     boolean isValidQuoteCharacter() {
         return quoteCharacter.length() == 1;
     }
+
+    @AssertTrue(message = "quote_character and delimiter cannot be the same character")
+    boolean areDelimiterAndQuoteCharacterDifferent() {
+        return !(delimiter.equals(quoteCharacter));
+    }
 }
