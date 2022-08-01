013f64d8736fd1d1a51ba8bac1b90a6f410033c4
Finn Roblin <finnrobl@amazon.com>
2022-08-01 14:43:55 -0700

    Removed final keyword in newline codec config skipLines
    Signed-off-by: Finn Roblin <finnrobl@amazon.com>

--- data-prepper-plugins/s3-source/src/main/java/com/amazon/dataprepper/plugins/source/codec/NewlineDelimitedConfig.java
+++ data-prepper-plugins/s3-source/src/main/java/com/amazon/dataprepper/plugins/source/codec/NewlineDelimitedConfig.java
@@ -9,7 +9,8 @@ package com.amazon.dataprepper.plugins.source.codec;
  * Configuration class for the newline delimited codec.
  */
 public class NewlineDelimitedConfig {
-    private final int skipLines = 0;
+    static final int DEFAULT_SKIP_LINES = 0;
+    private int skipLines = DEFAULT_SKIP_LINES;
 
     /**
      * The number of lines to skip from the start of the S3 object.
